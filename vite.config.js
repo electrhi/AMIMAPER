c265f07aae4a5e8a9bd692d17a754cf0db6854f1
electrhi <electrichi3882@gmail.com>
2025-11-06 10:49:46 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
-  build: { outDir: "dist" }
+  root: ".", // ✅ index.html의 위치를 명시
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
 });
